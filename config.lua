b24295b7f661ca2ccab04e0f215f9d9bb3fe48f3
Jan Bronicki <janbronicki@gmail.com>
2024-01-25 20:31:30 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,15 +1,17 @@
 lvim.use_icons = true
-
 lvim.builtin.nvimtree.setup.view.side = "right"
+
 lvim.plugins = {
-  {"lunarvim/colorschemes"},
-  {"Mofiqul/vscode.nvim"},
-  {"folke/tokyonight.nvim"},
-  {"psliwka/vim-smoothie"},
-  {"kkoomen/vim-doge"},
+
+    {"lunarvim/colorschemes"},
+    {"Mofiqul/vscode.nvim"},
+    {"folke/tokyonight.nvim"},
+    {"psliwka/vim-smoothie"},
+    {"kkoomen/vim-doge"},
 }
 
 
 lvim.colorscheme = "vscode"
-
 lvim.reload_config_on_save = true
+vim.diagnostic.config({ virtual_text = false })
+
